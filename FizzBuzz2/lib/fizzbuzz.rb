aaa355660431068d387a0268af85b5394411e142
Josh Bebbington <josh@focalwebsites.com>
2014-12-04 15:06:10 +0000

    Completed fizzbuzz? method

--- FizzBuzz2/lib/fizzbuzz.rb
+++ FizzBuzz2/lib/fizzbuzz.rb
@@ -13,3 +13,10 @@ end
 def is_divisible_by_15?(number)
 	is_divisible_by?(number, 15)
 end
+
+def fizzbuzz?(number)
+	return "FizzBuzz!" if is_divisible_by_15?(number)
+	return "Fizz!" if is_divisible_by_3?(number)
+	return "Buzz!" if is_divisible_by_5?(number)
+	number
+end
--- FizzBuzz2/spec/fizzbuzz_spec.rb
+++ FizzBuzz2/spec/fizzbuzz_spec.rb
@@ -30,4 +30,28 @@ describe 'fizzbuzz' do
 			expect(is_divisible_by_15?(1)).not_to be true
 		end
 	end
+
+	context 'while playing the game' do
+		it 'returns fizz for 3' do
+			expect(fizzbuzz?(3)).to eq "Fizz!"
+		end
+
+		it 'returns buzz for 5' do
+			expect(fizzbuzz?(5)).to eq "Buzz!"
+		end
+
+		it 'returns fizzbuzz for 15' do
+			expect(fizzbuzz?(15)).to eq "FizzBuzz!"
+		end
+
+		it 'returns number for other numbers' do
+			expect(fizzbuzz?(1)).to eq 1
+		end
+	end
 end
+
+
+
+
+
+
